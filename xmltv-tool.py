6ace834d14474f11a88b20299e720a1495043455
Pau Minoves <pau.minoves@gmail.com>
2016-04-09 01:45:46 +0200

    accumulate is gone

--- xmltv-tool.py
+++ xmltv-tool.py
@@ -26,14 +26,14 @@ def accumulate_channel(channel):
         channel_accumulate[channel.attrib['id']] = 1
 
 def print_accumulate():
-    print('Number of channels: ' + str(len(channel_accumulate)))
-    print('Programs per day: ')
+    print('channels: ' + str(len(channel_accumulate)))
+    print('programs: ')
     for Y in stats_accumulate:
         for M in stats_accumulate[Y]:
             for D in stats_accumulate[Y][M]:
                 print('\t{0} {1} {2}: {3}'.format(Y,M,D, stats_accumulate[Y][M][D]))
 
-def do_stats(xmltv, accumulate):
+def do_stats(xmltv):
     global channel_count
     channels = xmltv.findall('./channel')
     programs = xmltv.findall('./programme')
@@ -46,33 +46,40 @@ def do_stats(xmltv, accumulate):
         start = datetime.strptime( program.attrib['start'], '%Y%m%d%H%M%S %z')
         accumulate_by_date(start.year, start.month, start.day)
 
-    if not accumulate:
-        print_accumulate()
-        stats_accumulate.clear()
-        channel_accumulate.clear()
-        channel_count = 0
-
 def main(stats: ('Print stats about the files instead of the resulting file','flag','i'),
         shift_dates: ('Shift the start time dates','option','s'),
         utc: ('Normalize start time to UTC','option','u'),
-        accumulate: ('Accumulate the stats instead of printing each file separatelly. Implies -a', 'flag', 'I'),
         *xmltv_files):
-    "Utility to inspect and manipulate XMLTV files"
+    """
+    Utility to inspect and manipulate XMLTV files.
+
+    Input files are merged in the output.
+    """
+
+    # Input
+
+    xmltv = ET.parse(xmltv_files[0]).getroot()
+    for xmltv_file in xmltv_files[1:]:
+        one_xmltv = ET.parse(xmltv_file).getroot()
+        for elem in one_xmltv:
+            xmltv.extend(elem)
+
+    # Process
 
 
-    if accumulate: # -A implies -a
-        stats = True
+
+    # Output
 
     if stats:
         for xmltv_file in xmltv_files:
             xmltv = ET.parse(xmltv_file)
-            do_stats(xmltv, accumulate)
-        if accumulate:
-            print_accumulate()
-            print('Total number of channels: ' + str(channel_count))
+            do_stats(xmltv)
+        print_accumulate()
 
 if  __name__ == '__main__':
     try:
         import plac; plac.call(main)
+#    except (FileNotFoundError, ValueError) as e:
     except FileNotFoundError as e:
         print(e)
+
